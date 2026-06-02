908fd2335bd305a0114bd4046b5d97ad917ec06f
Faiyaz <mk3590653@gmail.com>
2026-06-02 07:27:58 +0600

    Added new functionality to app.js

--- Day-01/app.js
+++ Day-01/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
 
+const notes = [];
+
 app.get('/', (req,res)=>{
     res.send('Hi, Faiyaz!')
+})
+
+app.post('/notes', (req,res)=>{
+    res.send('notes created!')
 })
 
 app.listen(3000)
