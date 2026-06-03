85a61dc8a0482966ed298e3465706af6eecde5b4
Faiyaz <mk3590653@gmail.com>
2026-06-03 08:09:15 +0600

    try with put, patch, delete methods

--- Day-01/app.js
+++ Day-01/app.js
@@ -1,14 +1,31 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const notes = [];
 
-app.get('/', (req,res)=>{
-    res.send('Hi, Faiyaz!')
-})
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Hi, Faiyaz!");
+});
 
-app.post('/notes', (req,res)=>{
-    res.send('notes created!')
-})
+app.post("/notes", (req, res) => {
+  console.log(req.body);
+  notes.push(req.body);
+  res.send("notes created!");
+});
 
-app.listen(3000)
+app.get("/notes", (req, res) => {
+  res.send(notes);
+});
+
+app.delete("/notes/:id", (req, res) => {
+  delete notes[req.params.id];
+  res.send("note deleted sucessfully");
+});
+
+app.patch("/notes/:id", (req, res) => {
+  notes[req.params.id].description = req.body.description;
+  res.send("notes updated properly");
+});
+
+app.listen(3000);
