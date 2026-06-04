76e4ac1388f21dbac748e260e8c643788cf6b19e
Faiyaz <mk3590653@gmail.com>
2026-06-04 10:04:57 +0600

    feat: add create, read, update and delete endpoints for notes

--- Day-02/server.js
+++ Day-02/server.js
@@ -1,15 +1,5 @@
 const app = require("./src/app");
 
-const notes = [];
-app.post("/notes", (req,res)=>{
-    res.send("Faiyaz Khan created a note properly")
-})
-
-app.get("/notes", (req,res)=>{
-    res.send("Faiyaz Khan created a note properly")
-
-})
-
 
 app.listen(3000, () => {
   console.log("server is running on port 3000");
