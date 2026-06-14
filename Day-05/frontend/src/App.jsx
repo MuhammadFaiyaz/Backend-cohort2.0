d6dce9ebf000bc0179e991379d3b2ba10fd08c2f
Faiyaz <mk3590653@gmail.com>
2026-06-14 07:09:01 +0600

    Integrated frontend

--- Day-05/frontend/src/App.jsx
+++ Day-05/frontend/src/App.jsx
@@ -1,39 +1,82 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios'
 import './App.css';
+
+
 const App = () => {
 
-  const [notes, setNotes] = useState([
-    {
-      title: "This is title 1",
-      description: "This is description 1"
-    },
-    {
-      title: "This is title 2",
-      description: "This is description 2"
-    },
-    {
-      title: "This is title 3",
-      description: "This is description 3"
-    },
-    {
-      title: "This is title 4",
-      description: "This is description 4"
-    },
-  ])
+  const [notes, setNotes] = useState([])
+  const [editId, setEditId] = useState(null)
+  const [description, setDescription] = useState("")
 
-  axios.get("http://localhost:3000/api/notes")
-    .then((res) => {
-      setNotes(res.data.notes)
+  const fetchNotes = async () => {
+    axios.get("http://localhost:3000/api/notes")
+      .then((res) => {
+        setNotes(res.data.notes)
+      })
+  }
+
+  useEffect(() => {
+    fetchNotes();
+  }, [])
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const { title, description } = e.target.elements;
+    axios.post("http://localhost:3000/api/notes", {
+      title: title.value,
+      description: description.value
+    }).then((res) => {
+      console.log(res.data);
+      fetchNotes();
     })
+  }
+
+  const deleteHandler = (noteId) => {
+    axios.delete("http://localhost:3000/api/notes/" + noteId)
+      .then(res => {
+        console.log(res.data);
+        fetchNotes()
+      })
+    console.log(noteId);
+  }
+
+  const updateHandler = async () => {
+    try {
+      await axios.patch(`http://localhost:3000/api/notes/${editId}`, {
+        description
+      });
+      fetchNotes();
+      setEditId(null);
+      setDescription("");
+    } catch (err) {
+      console.log(err);
+    }
+
+  }
 
   return (
     <>
+      <form className='note-create-form' onSubmit={submitHandler}>
+        <input name="title" type="text" placeholder='Title' />
+        <input name="description" value={description} onChange={(e) => setDescription(e.target.value)} type="text" placeholder='Description' />
+        <div className="btn-container">
+          <button type="submit">Create Note</button>
+          <button type="button" onClick={updateHandler}>Update Note</button>
+        </div>
+      </form>
       <div className="notes">
-        {notes.map(note => (
-          <div className="note">
+        {Array.isArray(notes) && notes.map(note => (
+          <div className="note" key={note._id}>
             <h1>{note.title}</h1>
             <p>{note.description}</p>
+            <div className="btn-container">
+              <button className="delete-button" onClick={() => { deleteHandler(note._id) }}>Delete</button>
+              <button className="edit-button" onClick={() => {
+                setDescription(note.description)
+                setEditId(note._id)
+              }}>Edit</button>
+            </div>
           </div>
         ))}
       </div>
