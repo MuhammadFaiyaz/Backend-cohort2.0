90dbb4caf0eade90254f2f400915f0568747c33c
Faiyaz <mk3590653@gmail.com>
2026-06-20 09:07:21 +0600

    Another practise done

--- Day-07(auth)/server.js
+++ Day-07(auth)/server.js
@@ -1,9 +1,8 @@
 require("dotenv").config();
-const app = require("./src/app")
+const app = require("./src/app");
 const connectToDb = require("./src/config/db");
 
-connectToDb();
-
-app.listen(3000, ()=>{
-    console.log("Server is running on port 3000");
-})
+connectToDb()
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
