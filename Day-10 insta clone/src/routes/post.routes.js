d6a6dfd15b25582d5692acf0bbad79b72258b027
Faiyaz <mk3590653@gmail.com>
2026-06-25 12:13:44 +0600

    Insta clone add post feature

--- Day-10 insta clone/src/routes/post.routes.js
+++ Day-10 insta clone/src/routes/post.routes.js
@@ -1,9 +1,11 @@
-const {createPostController} = require("../controller/post.controller");
+const {createPostController, getPostController, getPostDeatailsController} = require("../controller/post.controller");
 const multer = require("multer");
 const upload = multer({ storage: multer.memoryStorage() });
 const express = require("express");
 const postRouter = express();
 
 postRouter.post("/", upload.single("image"),createPostController);
+postRouter.get("/", getPostController);
+postRouter.get("/details/:postId", getPostDeatailsController);
 
 module.exports = postRouter;
