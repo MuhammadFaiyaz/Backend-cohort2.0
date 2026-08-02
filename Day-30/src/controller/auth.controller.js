e46ca18cf70e31e4beda9c5763d8548d8f0985ba
Faiyaz <mk3590653@gmail.com>
2026-08-02 06:57:39 +0600

    Add user registration validation and update registration route

--- Day-30/src/controller/auth.controller.js
+++ Day-30/src/controller/auth.controller.js
@@ -1,8 +1,12 @@
 export const registerUser = ( req, res, next) => {
-    try {
-        throw new Error("Password is too weak.");
-    } catch (err) {
-        err.status = 400;
-        next(err);
-    }
+    // try {
+    //     throw new Error("Password is too weak.");
+    // } catch (err) {
+    //     err.status = 400;
+    //     next(err);
+    // }
+
+    res.status(201).json({
+        message: "User registered successfully"
+    })
 }
--- Day-30/src/routes/auth.routes.js
+++ Day-30/src/routes/auth.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 const authRouter = Router();
 import { registerUser } from "../controller/auth.controller.js";
+import { registerUserValidation } from "../validator/auth.validator.js";
 
-authRouter.post("/register", registerUser)
+authRouter.post("/register", registerUserValidation, registerUser)
 
 export default authRouter;
