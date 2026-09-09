03e46918f3f0be9f5ac9c1c5df9b04037e8d7eb6
Faiyaz <mk3590653@gmail.com>
2026-09-09 07:08:14 +0600

    feat: add Zod validation and integrate new solutions in graph service

--- Ai battle arena/src/services/graph.ai.service.ts
+++ Ai battle arena/src/services/graph.ai.service.ts
@@ -1,18 +1,44 @@
 import { HumanMessage } from "@langchain/core/messages";
 import { StateSchema, MessagesValue, ReducedValue, StateGraph, START, END } from "@langchain/langgraph";
 import type { GraphNode } from "@langchain/langgraph";
+import { z } from "zod";
+import { cohereModel, mistralModel } from "./models.service.js";
+
 const State = new StateSchema({
-    messages: MessagesValue
+    messages: MessagesValue,
+    solution_1: new ReducedValue(z.string().default(""), {
+        reducer: (current, next) => {
+            return next
+        }
+    }),
+    solution_2: new ReducedValue(z.string().default(""), {
+        reducer: (current, next) => {
+            return next
+        }
+    }),
+    judge_recommendation: new ReducedValue(z.object().default({
+        solution_score_1: 0,
+        solution_score_2: 0
+
+    }), {
+        reducer: (current, next) => {
+            return next
+        }
+    })
 })
 
-const solutionNode: GraphNode<typeof State> = (state: typeof State) => {
-    console.log(state);
-    return{
-      messages: state.messages[0]  
-    } 
+const solutionNode: GraphNode<typeof State> = async (state: typeof State) => {
+    const [mistral_solution, cohere_solution] = await Promise.all([
+        mistralModel.invoke(state.messages[0].text),
+        cohereModel.invoke(state.messages[0].text),
+    ])
+    return {
+        solution_1: mistral_solution.text,
+        solution_2: cohere_solution.text
+    }
 }
 
-const graph = new StateGraph(State).addNode("solution", solutionNode).addEdge(START, "solution").compile()
+const graph = new StateGraph(State).addNode("solution", solutionNode).addEdge(START, "solution").addEdge("solution", END).compile()
 
 export default async function (userMessage: string) {
     const result = await graph.invoke({
@@ -20,6 +46,5 @@ export default async function (userMessage: string) {
             new HumanMessage(userMessage)
         ]
     })
-
     return result.messages
 }
--- Ai battle arena/src/services/models.service.ts
+++ Ai battle arena/src/services/models.service.ts
@@ -1,22 +1,24 @@
 import { ChatGoogle } from "@langchain/google";
 import { ChatMistralAI } from "@langchain/mistralai"
 import { ChatCohere } from "@langchain/cohere"
-import config from "../config/config.js";
+import app_config from "../config/config.js";
 
 
 const geminiModel = new ChatGoogle({
     model: "gemini-3.7-flash",
-    apiKey: config.GOOGLE_API_KEY,
+    apiKey: app_config.GOOGLE_API_KEY,
 });
 
 const mistralModel = new ChatMistralAI({
     model: "mistral-medium-latest",
-    apiKey: config.MISTRAL_API_KEY
+    apiKey: app_config.MISTRAL_API_KEY
     
 })
 
 const cohereModel = new ChatCohere({
     model: "command-a-03-2025",
-    apiKey: config.COHERE_API_KEY
+    apiKey: app_config.COHERE_API_KEY
 
 })
+
+export { geminiModel, mistralModel, cohereModel }
