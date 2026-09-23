a92f78e494e6c4704129f4ed87fdb3f05251e72a
Faiyaz <mk3590653@gmail.com>
2026-09-23 10:54:35 +0600

    feat: initialize frontend with React, Vite, and Tailwind CSS
    - Added package.json for project dependencies and scripts
    - Created favicon.svg for the application
    - Added icons.svg containing various SVG icons
    - Implemented main application component (App.jsx)
    - Included Tailwind CSS in index.css
    - Set up entry point (main.jsx) for React application
    - Configured Vite with React plugin in vite.config.js

--- SNITCH/Backend/src/controllers/auth.controller.js
+++ SNITCH/Backend/src/controllers/auth.controller.js
@@ -2,10 +2,26 @@ import userModel from "../models/user.model.js";
 import jwt from "jsonwebtoken";
 import { config } from "../config/config.js";
 
-const sendTokenResponse = (req, res) => {
+const sendTokenResponse = async (user, res, message) => {
   const token = jwt.sign({
-    id: req.user._id,
-  }, config.JWT_SECRET)
+    id: user._id,
+  }, config.JWT_SECRET, {
+    expiresIn: '7d'
+  })
+
+  res.cookie('token', token)
+
+  res.status(200).json({
+    message,
+    token,
+    user: {
+      id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      contactNumber: user.contactNumber,
+      role: user.role
+    }
+  })
 }
 
 export const register = async (req, res) => {
@@ -27,7 +43,7 @@ export const register = async (req, res) => {
       fullName,
     });
 
-
+   await sendTokenResponse(user, res, "User registered successfully");
 
   } catch (error) {
     res.status(500).json({ message: "Server error", error: error.message });
--- SNITCH/Backend/src/routes/auth.routes.js
+++ SNITCH/Backend/src/routes/auth.routes.js
@@ -1,9 +1,9 @@
 import {Router} from "express";
 import { validateRegister } from "../validator/auth.validator.js";
-
+import { register } from "../controllers/auth.controller.js";
 const authRouter = Router();
 
-authRouter.get("/register", validateRegister );
+authRouter.post("/register", validateRegister, register );
 // authRouter.get("/login", );
 
 export default authRouter;
